2e24010dc9e69248b1e98a4670fbf7bfa2599609
Jean-François Roche <jfroche@affinitic.be>
2012-02-22 22:24:06 +0100

    add rpmforge

--- manifests/client.pp
+++ manifests/client.pp
@@ -20,17 +20,31 @@ class yum::client {
   file {
     "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-$rel":
       ensure => present,
-      source => "puppet:///modules/yum/RPM-GPG-KEY-EPEL-$rel",
+      source => "puppet:///modules/yum/RPM-GPG-KEY-EPEL-$rel";
   }
+
+  file {
+    "/etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-$rel":
+      ensure => present,
+      source => "puppet:///modules/yum/RPM-GPG-KEY-rpmforge-$rel";
+  }
+
 
   yumrepo {
     'epel':
       descr    => 'Extra Packages for Enterprise Linux $releasever',
-      baseurl  => "http://download.fedora.redhat.com/pub/epel/\$releasever/$::architecture",
+      baseurl  => "http://dl.fedoraproject.org/pub/epel/\$releasever/$::architecture",
       enabled  => 1,
       gpgcheck => 1,
       gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-$rel",
-      require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-$rel"],
+      require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-$rel"];
+    'rpmforge':
+      descr    => 'RHEL $releasever - RPMforge.net - dag',
+      baseurl  => 'http://apt.sw.be/redhat/el6/en/$basearch/rpmforge',
+      enabled  => 1,
+      gpgcheck => 1,
+      gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-$rel",
+      require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-$rel"];
   }
 
 
