358b6759172572fd95087083b244d513349a38a9
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-04-05 16:15:19 +0200

    add doc to cirbrepo

--- manifests/cirbrepo.pp
+++ manifests/cirbrepo.pp
@@ -1,3 +1,17 @@
+# == Define: cirbrepo
+#
+# Add a repo located in CIRB infra
+#
+# === Parameters
+# [*url*]
+#   Url of the yum repo
+#
+# [*descr*]
+#   Description of yum repo
+#
+# [*enable*]
+#   0 is diabled en 1 is enbabled
+#
 define yum::cirbrepo($url, $descr, $enabled=1) {
 
   yumrepo {"cirb-repo-${name}":
